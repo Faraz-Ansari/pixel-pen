2ab4e331b168ede2d9a171ca40216b8bafcb92f2
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-11-28 23:11:04 +0530

    "Added react-router-dom dependency and implemented routing in App.jsx"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+
 export default function App() {
-    return <h1 className=" font-bold hover:underline transition-all">Hello world!</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
