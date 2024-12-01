f7077f9bbb7381232f1c8a01ce910b379387622a
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-12-01 23:16:51 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to database successfully");
+    })
+    .catch((err) => {
+        console.error(err.message);
+    });
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
