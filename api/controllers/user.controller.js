bd006234621e4ab0a90e776eded6ea59e651083f
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-12-10 21:05:54 +0530

    create delete user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,10 +52,26 @@ export const updateUser = async (req, res, next) => {
             { new: true }
         );
 
-        const {password, ...restOfUser} = user._doc;
+        const { password, ...restOfUser } = user._doc;
 
         res.status(200).json(restOfUser);
     } catch (error) {
         return next(error);
+    }
+};
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(
+            errorHandler("You are not authorized to perform this action", 403)
+        );
+    }
+
+    try {
+        await User.findByIdAndDelete(req.params.id);
+
+        res.status(200).json("User has been deleted successfully");
+    } catch (error) {
+        next(error);
     }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
