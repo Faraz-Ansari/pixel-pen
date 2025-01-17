9b3564fb054d7195ec40b79c8ac7009222cf4648
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2025-01-18 00:45:43 +0530

    complete About and Projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,36 @@
 export default function About() {
-    return <div>About</div>;
+    return (
+        <div className="flex items-center justify-center my-2">
+            <div className="max-w-3xl mx-auto p-3">
+                <h1 className="text-3xl md:text-4xl font-semibold text-center my-5">
+                    About Pixel Pen
+                </h1>
+                <div className="text-lg flex flex-col gap-4">
+                    <p>
+                        Welcome to Pixel Pen! This blog was created by Ahmad
+                        Faraz Ansari as a personal project to share his thoughts
+                        and ideas with the world. Faraz is a passionate
+                        developer who loves to write about technology, coding,
+                        and everything in between.
+                    </p>
+
+                    <p>
+                        On this blog, you'll find weekly articles and tutorials
+                        on topics such as web development, software engineering,
+                        and programming languages. Faraz is always learning and
+                        exploring new technologies, so be sure to check back
+                        often for new content!
+                    </p>
+
+                    <p>
+                        We encourage you to leave comments on our posts and
+                        engage with other readers. You can like other people's
+                        comments and reply to them as well. We believe that a
+                        community of learners can help each other grow and
+                        improve.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,3 +1,19 @@
+import CallToAction from "../components/CallToAction";
+
 export default function Projects() {
-    return <div>Projects</div>;
+    return (
+        <div className="flex items-center justify-center my-2">
+            <div className="max-w-3xl mx-auto p-3">
+                <h1 className="text-3xl md:text-4xl font-semibold text-center my-5">
+                    Projects
+                </h1>
+                <p className="text-lg">
+                    Build fun and engaging projects while learning HTML, CSS,
+                    and TypeScript! <br />
+                    Follow the link below to view the projects.
+                </p>
+                <CallToAction />
+            </div>
+        </div>
+    );
 }
